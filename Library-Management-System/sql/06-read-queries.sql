0afbf4fed89a04a985fd00dcbd7517e36b54dd9b
Kgwale NN <kgoalemothiba8@gmail.com>
2026-09-25 10:01:21 +0200

    Add library read queries

---
+++ Library-Management-System/sql/06-read-queries.sql
@@ -0,0 +1,16 @@
+-- Get all books
+SELECT * FROM books;
+
+-- Get a book by title
+SELECT * FROM books
+WHERE title = '1984';
+
+-- Get all books by a specific author
+SELECT books.*
+FROM books
+JOIN authors ON books.author_id = authors.id
+WHERE authors.name = 'George Orwell';
+
+-- Get all available books
+SELECT * FROM books
+WHERE available = TRUE;
